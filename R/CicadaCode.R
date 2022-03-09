ac6b36dd7b5712873fb223e7c47f84e44eb99593
Allison Roth <amr2264@columbia.edu>
2022-03-09 16:07:33 -0500

    Fig Legends

--- R/CicadaCode.R
+++ R/CicadaCode.R
@@ -896,7 +896,7 @@ ggplot() +
   theme_bw() +
   scale_color_manual(values = c("blue", "red")) +
   theme(legend.position = "none")  +
-  labs(y = "Successful Copulation (Yes = 1 & No = 0)", x = "Exploratory Beahviour Score (z transformed)")
+  labs(y = "Successful Copulation (Yes = 1 & No = 0)", x = "Exploratory Behavior Score (z transformed)")
 
 # #Switch Pen 1B back to Pen B
 # I$Pen[I$Pen == "1B"] <- "B"
@@ -964,7 +964,7 @@ ggplot() +
   theme_bw() +
   scale_color_gradient() +
   theme(legend.position = "none")  +
-  labs(y = "Number of Male-Male Copulatoin Attempts", x = "Exploratory Beahviour Score (z transformed)")
+  labs(y = "Number of Male-Male Copulation Attempts", x = "Exploratory Behavior Score (z transformed)")
 
 
 
@@ -1056,7 +1056,7 @@ ggplot() +
   theme_bw() +
   scale_color_gradient() +
   theme(legend.position = "none")  +
-  labs(y = "Number of All Copulatoins", x = "Exploratory Beahviour Score (z transformed)")
+  labs(y = "Number of All Attemptd Copulations", x = "Exploratory Behavior Score (z transformed)")
 
 
 
